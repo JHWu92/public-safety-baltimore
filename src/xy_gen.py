014026f320b430c4ba30c27c7c62f8d889252f16
JHWu92 <jhwu92@gmail.com>
2018-04-17 09:03:55 -0400

    fix bug

--- src/xy_gen.py
+++ src/xy_gen.py
@@ -1,14 +1,16 @@
 import geopandas as gp
+from shapely.geometry import Point
 
 def y_cnt_event(spatial_units, events):
     """
 
     :param spatial_units: gp.GeoDataFrame
-    :param events: gp.GeoDataFrame
+    :param events: pd.Series of coords, the coords should be in the same crs of spatial units
     :return:
     """
-
-    joined = gp.sjoin(events.reset_index(), spatial_units)
-    y_cnt =spatial_units.join(joined.groupby('index_right').agg({'index':'count'}), how='left')\
-                    .rename(columns={'index':'count'}).fillna(0)
-    return y_cnt[['count']]
+    events = gp.GeoDataFrame(events.apply(lambda x: Point(*x))).rename(columns={'coords': 'geometry'}).reset_index()
+    while events.crs is None and spatial_units.crs is not None:
+        events.crs = spatial_units.crs
+    joined = gp.sjoin(events, spatial_units)
+    y_cnt = spatial_units.join(joined.groupby('index_right').size().rename('counts'), how='left').fillna(0)
+    return y_cnt['counts']
