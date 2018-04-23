401e195cac7951da72af5a789c6f8bb796a7a6e4
JHWu92 <jhwu92@gmail.com>
2018-04-23 16:09:28 -0400

    more constants

--- src/constants.py
+++ src/constants.py
@@ -1,22 +1,30 @@
 class COL:
     """column names
+
         Attributes:
         ----------
+
+        other:
+
+        - ori_index: name for the original index of the raw data file
+
         Datetime related:
 
-        date: Date column for timestamped dataframe
-        date_format: the string format of Date column
-        time: Time column for timestamped dataframe
-        time_format: the string format of Time column
+        - date: Date column for timestamped dataframe
+        - date_format: the string format of Date column
+        - time: Time column for timestamped dataframe
+        - time_format: the string format of Time column
 
         Spatial related:
 
-        lat: Latitude
-        lon: Longitude
-        coords: coordinate of points, (lat, lon) or (X, Y) in other CRS
-        center: the coordinate of the center of a geometry object
-        category: category column
+        - lat: Latitude
+        - lon: Longitude
+        - coords: coordinate of points, (lon, lat) or (X, Y) in other CRS
+        - center: the coordinate of the center of a geometry object
+        - category: category column
     """
+    # other
+    ori_index = 'ori_index'
     # datetime related
     date = 'Date'
     date_format = '%Y-%m-%d'
@@ -28,3 +36,14 @@ class COL:
     coords = 'Coords'
     center = 'Cen_coords'
     category = 'Category'
+
+
+class PATH_DEV:
+    """
+    path names of clean dev set data
+
+    Attributes
+    ----------
+    p911: 911 data
+    """
+    p911 = 'data/open-baltimore/clean/911-dev-set.csv'
