47ae9ff037d153ebd67e2fb508dc32a5c665096b
JHWu92 <jhwu92@gmail.com>
2018-04-30 00:10:35 -0400

    clean crime

---
+++ data/open-baltimore/clean_crime.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+import pandas as pd
+from manual.category_mapping import CatMapping
+import sys
+
+
+def main():
+    sys.path.append("../../src")
+    from constants import COL
+
+    crimes = pd.read_csv('raw/BPD_Part_1_Victim_Based_Crime_Data.csv')
+    crimes = crimes[~((crimes.Longitude.isnull()) | (crimes.Latitude.isnull()))].copy()
+    # parse datetime
+    crimes[COL.date] = crimes['CrimeDate']
+    crimes[COL.date] = pd.to_datetime(crimes[COL.date], format='%m/%d/%Y')
+
+    # There are some full-line duplicates
+    crimes = crimes[~crimes.duplicated()].copy()
+
+    print('map categories')
+    cmap = CatMapping('manual/crime_categories.csv')
+    crimes[cmap.to_col] = cmap.apply_mapping(crimes)
+
+    # clean noise
+    crimes['In/Outside'] = crimes['Inside/Outside'].apply(
+        lambda x: 'O' if x == 'Outside' else 'I' if x == 'Inside' else x)
+
+    # drop unused columns
+    crimes.drop(['Location 1', 'Total Incidents', 'Inside/Outside', 'CrimeDate'], axis=1, inplace=True)
+    crimes.rename(columns={'CrimeTime': COL.time}, inplace=True)
+
+    print('split dev/test set')
+    crimes[crimes.Date < pd.datetime.strptime('2017-01-01', '%Y-%m-%d')].to_csv('clean/crimes-dev-set.csv')
+    crimes[crimes.Date >= pd.datetime.strptime('2017-01-01', '%Y-%m-%d')].to_csv('clean/crimes-test-set.csv')
+
+    return
+
+
+if __name__ == '__main__':
+    main()
