f445be263e3447c364ae9006d45692e5f18d669b
JHWu92 <jhwu92@gmail.com>
2018-08-09 15:57:38 -0400

    add set_x

--- src/e1_compile_data.py
+++ src/e1_compile_data.py
@@ -1,9 +1,22 @@
 from src.constants import PathData, COL
+from itertools import chain
 from src.data_prep import prep_911, prep_crime
 
 
 def df_categories(df):
     return df[COL.category].unique()
+
+
+def subdf_by_categories(df, categories):
+    """
+
+    :param df:
+    :param categories: str, or list
+    :return:
+    """
+    if isinstance(categories, str):
+        return df[df[COL.category] == categories]
+    return df[df[COL.category].isin(categories)]
 
 
 def load_911():
@@ -75,8 +88,9 @@ class Data:
 class CompileData:
     def __str__(self):
         return ('Compile Data Module:\n'
+                '\t- {x_setting}\n'
                 '\t- {y_setting}\n'
-                ''.format(y_setting=self.data_y)
+                ''.format(x_setting=self.data_x, y_setting=self.data_y)
                 )
 
     def __init__(self, verbose=0):
@@ -90,13 +104,14 @@ class CompileData:
         """select data loading function by name"""
 
         if dname not in LOAD_FUNCS:
-            raise ValueError(
-                'Name=%s cannot be loaded, it hasnt been implemented. Supported data: %s' % (
-                    dname, ', '.join(LOAD_FUNCS.keys())
-                )
-            )
-        func = LOAD_FUNCS[dname]
+            raise ValueError('Name=%s cannot be loaded, it hasnt been implemented. Supported data: %s' % (
+                dname, ', '.join(LOAD_FUNCS.keys()))
+                             )
 
+        if self.verbose:
+            print('loading data ' + dname)
+
+        func = LOAD_FUNCS[dname]
         train, dev = func()
         self._data_loaded.set_tr_de(dname, train, dev)
 
@@ -144,8 +159,62 @@ class CompileData:
         if self.verbose >= 1:
             print('set data for y, dname/categories=%s' % dname_cat)
 
-    def set_x(self, dnames, by_catogory=True):
-        pass
+    def set_x(self, dnames, by_category=True, category_groups=None):
+        """Set data for X.
+
+        :param dnames: list.
+            List of dnames to be used to generate feature X.
+        :param by_category: bool or list of Boolean, default True.
+            whether or not to divide the data by categories.
+            Specify list for multiple datasets.
+            If this is a list of bools, must match the length of the dnames.
+        :param category_groups: dict
+            key: dname;
+            value: list of groups of categories.
+            define categories that should be treated as groups for each dname.
+        """
+        if not isinstance(by_category, (tuple, list)):
+            by_category = [by_category] * len(dnames)
+        assert len(by_category) == len(dnames), 'Len of by_category != dnames'
+
+        for dname, byc in zip(dnames, by_category):
+            self.is_loaded(dname)
+            has_group = category_groups is not None and dname in category_groups
+            groups = category_groups[dname] if has_group else []
+            cat_in_g = set(chain(*groups))
+
+            # categories in groups are treated as homogeneous
+            if has_group:
+                if self.verbose:
+                    print('set_x: adding groups for data ' + dname)
+                for g in groups:
+                    dname_cat = '%s/%s' % (dname, '+'.join(g))
+                    self.data_x.set_tr_de(dname_cat,
+                                          subdf_by_categories(self._data_loaded.tr[dname], g),
+                                          subdf_by_categories(self._data_loaded.de[dname], g))
+
+            # each category not in groups is treated as a set of independent point
+            if byc:
+                categories = set(df_categories(self._data_loaded.tr[dname])) | set(
+                    df_categories(self._data_loaded.de[dname]))
+
+                if self.verbose:
+                    print('set_x: adding individual category of data ' + dname)
+                    print('all categories in data: {} are: {}.'.format(dname, categories))
+                    if has_group:
+                        print('individual categories are: {}'.format(categories - cat_in_g))
+
+                for c in categories - cat_in_g:
+                    dname_cat = '%s/%s' % (dname, c)
+                    self.data_x.set_tr_de(dname_cat,
+                                          subdf_by_categories(self._data_loaded.tr[dname], c),
+                                          subdf_by_categories(self._data_loaded.de[dname], c))
+
+            # treated as one set of homogeneous points
+            if not has_group and not byc:
+                if self.verbose:
+                    print('set_x: adding whole set of data ' + dname)
+                self.data_x.set_tr_de(dname, self._data_loaded.tr[dname], self._data_loaded.de[dname])
 
     @property
     def y_train(self):
@@ -161,7 +230,10 @@ if __name__ == "__main__":
 
     os.chdir('..')
     print('current working directory' + os.getcwd())
-    compile_data = CompileData()
+    compile_data = CompileData(verbose=1)
     compile_data.set_y('crime')
     compile_data.set_y('crime/burglary')
     compile_data.set_y('crime/burglary+robbery')
+    compile_data.set_x(['crime', '911'], by_category=[True, False]
+                       # , category_groups={'crime': [['burglary', 'theft_larceny']]}
+                       )
