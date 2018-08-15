9561c409faf6a76f90978a6d9e696d531f713a8d
JHWu92 <jhwu92@gmail.com>
2018-08-15 18:10:47 -0400

    Train init

---
+++ src/e2_training.py
@@ -0,0 +1,66 @@
+from src.rolling_set import Rolling
+
+
+class Train:
+    """Train module for the experiment.
+    Initiate each train module for each model with given hyper-parameters(hps)
+    Model itself can have a strategy to tune its hps (e.g., sklearn's random search cv)
+    """
+
+    def __str__(self):
+        rolling = str(self.roller).replace('\t', '\t\t')
+        train_setting = 'Stack of roll slices' if self.stack_roll else 'The last roll slice'
+        return ('Training for {mname}:\n'
+                '\t- y: {y}\n'
+                '\t- x: {x}\n'
+                '\t- trained on: {train_setting}\n'
+                '\t- Model: {model}\n'
+                '\t- {rolling}\n'
+                ''.format(mname=self.model_name, model=self.model,
+                          x=self.x_setting, y=self.y_setting,
+                          train_setting=train_setting, rolling=rolling
+                          )
+                )
+
+    def __init__(self, model, model_name, rps, y_setting, x_setting, stack_roll=False):
+        """
+        :param model: model with given hps
+        :param rps: dict.
+            rolling parameters. 'roll_back'=True
+        :param x_setting: str
+            what type of x to generate from the data in tw_past
+        :param y_setting: str
+            what type of y to generate from the data in tw_pred
+        :param stack_roll: bool, default False
+            whether or not to stack roll slices to enlarge training set.
+            If True, roll thru the data and stack slices as one training set
+            If False, train on the last roll slice
+        """
+        self.model = model
+        self.model_name = model_name
+        self.rps = rps
+        self.roller = Rolling(**self.rps)
+        self.x_setting = x_setting
+        self.y_setting = y_setting
+        self.stack_roll = stack_roll
+
+    def train(self):
+        pass
+
+
+if __name__ == "__main__":
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import RandomizedSearchCV
+    from scipy.stats import randint as sp_randint
+
+    PD = {"max_depth": [3, None],
+          "max_features": sp_randint(1, 11),
+          "min_samples_split": sp_randint(2, 11),
+          "min_samples_leaf": sp_randint(1, 11),
+          "bootstrap": [True, False],
+          "criterion": ["gini", "entropy"]}
+
+    M = RandomizedSearchCV(RandomForestClassifier, param_distributions=PD, n_iter=20)
+    RPS = {'roll_sd': '2015-01-01', 'roll_ed': '2017-01-01'}
+    TR = Train(M, RPS, 'dummy y', 'dummy x')
+    print(TR)
