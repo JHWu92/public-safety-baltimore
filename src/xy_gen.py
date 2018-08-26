338e5cacbe628f55385ad71611acf9f859306dbb
JHWu92 <jhwu92@gmail.com>
2018-08-26 18:50:25 -0400

    add event_cnt

--- src/xy_gen.py
+++ src/xy_gen.py
@@ -1,5 +1,7 @@
 import geopandas as gp
+import pandas as pd
 from shapely.geometry import Point
+
 from src import constants as C
 
 
@@ -16,3 +18,29 @@ def y_cnt_event(spatial_units, coords):
     joined = gp.sjoin(events, spatial_units)
     y_cnt = spatial_units.join(joined.groupby('index_right').size().rename(C.COL.num_events), how='left').fillna(0)
     return y_cnt[C.COL.num_events]
+
+
+def xy_generator(compile_data, x_setting, y_setting):
+    pass
+
+
+def event_cnt(data):
+    """
+
+    :param data: dict
+        key: dname, value: dataframe with spu_assignment
+    :param spu_name: spatial unit
+    :return: pd.DataFrame
+        index.name = constants.COL.spu
+        columns = dnames
+    """
+    cnts = []
+    for dname, df in data.items():
+        cnt = df.groupby(C.COL.spu).size()
+        cnt.name = dname
+        cnts.append(cnt)
+    cnts = pd.concat(cnts, axis=1)
+    if isinstance(cnts, pd.Series):
+        cnts = cnts.to_frame()
+    cnts.fillna(0, inplace=True)
+    return cnts
