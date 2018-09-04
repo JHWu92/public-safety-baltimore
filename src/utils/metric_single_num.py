5748858ec3509fd29f8824213a1b8da1e7ce55ae
JHWu92 <jhwu92@gmail.com>
2018-09-03 20:18:59 -0400

    add rmse

--- src/utils/metric_single_num.py
+++ src/utils/metric_single_num.py
@@ -1,15 +1,19 @@
-from sklearn.metrics import mean_absolute_error, r2_score, auc
-
+from sklearn.metrics import mean_absolute_error, r2_score, auc, mean_squared_error
+from math import sqrt
 from . import p2f
 from .metric_roc_like import (search_efficient_rate_upct_wrap, prediction_accuracy_index_upct_wrap,
                               area_to_perimeter_ratio_upct_wrap, hit_rate_upct_wrap)
 
 
-def mse_wrap(y_true, y_pred, spu=None):
+def rmse(y_true, y_pred, spu=None):
+    return sqrt(mean_squared_error(y_true, y_pred))
+
+
+def mae(y_true, y_pred, spu=None):
     return mean_absolute_error(y_true, y_pred)
 
 
-def r2_wrap(y_true, y_pred, spu=None):
+def r2(y_true, y_pred, spu=None):
     return r2_score(y_true, y_pred)
 
 
@@ -35,5 +39,5 @@ def hit_rate_upct_auc(y_true, y_pred, spu):
     return curve2auc(hit_rate_upct_wrap(y_true, y_pred, spu))
 
 
-metrics = [mse_wrap, r2_wrap, search_efficient_rate_upct_auc, prediction_accuracy_index_upct_auc, hit_rate_upct_auc,
+metrics = [rmse, mae, r2, search_efficient_rate_upct_auc, prediction_accuracy_index_upct_auc, hit_rate_upct_auc,
            area_to_perimeter_ratio_upct_auc]
