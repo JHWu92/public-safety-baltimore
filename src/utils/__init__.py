062c01c1b8f110d24558c571098a0f3e414587b4
JHWu92 <jhwu92@gmail.com>
2018-10-06 13:31:47 -0400

    add loubar thres

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,25 @@
-import re
-from src.constants import DateTimeRelated as dtr, COL
 import datetime
+import re
+from copy import copy
+
+import numpy as np
+
+from src.constants import DateTimeRelated as dtr, COL
+
+
+def loubar_thres(arr, is_sorted=False):
+    if not is_sorted:
+        arr = copy(arr)
+        arr = sorted(arr)
+
+    lonrenz_y = arr.cumsum() / arr.sum()
+    lonrenz_y = np.insert(lonrenz_y, 0, 0)
+    x_axis = np.arange(lonrenz_y.size)/(lonrenz_y.size-1)
+    slope = (lonrenz_y[-1] - lonrenz_y[-2]) / (x_axis[-1] - x_axis[-2])
+    loubar = (slope - 1) / slope
+    arr_thres = arr[int(np.ceil((lonrenz_y.size - 1) * loubar) - 1)]
+
+    return loubar, arr_thres
 
 
 def p2f(x):
