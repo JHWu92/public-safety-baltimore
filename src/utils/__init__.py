9f830caa3bbcc1f7e45c987bcd6a29c4c1d8d085
JHWu92 <jhwu92@gmail.com>
2018-12-05 15:59:38 -0500

    add cohen's d, update report table format

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,6 +7,15 @@ import numpy as np
 from src.constants import DateTimeRelated as dtr, COL
 
 
+# correct if the population S.D. is expected to be equal for the two groups.
+def cohen_d(x, y):
+    from numpy import std, mean, sqrt
+    nx = len(x)
+    ny = len(y)
+    dof = nx + ny - 2
+    return (mean(x) - mean(y)) / sqrt(((nx - 1) * std(x, ddof=1) ** 2 + (ny - 1) * std(y, ddof=1) ** 2) / dof)
+
+
 def str_is_float(s):
     try:
         float(s)
